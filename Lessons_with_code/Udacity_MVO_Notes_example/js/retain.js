64de32c2479e78245adc102876e20abe10551a0c
MomokoXu <momoko0808@hotmail.com>
2017-06-13 16:22:45 -0700

    add date to the example as exercise

---
+++ Lessons_with_code/Udacity_MVO_Notes_example/js/retain.js
@@ -0,0 +1,69 @@
+$(function(){
+
+    var model = {
+        init: function() {
+            if (!localStorage.notes) {
+                localStorage.notes = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.notes);
+            data.push(obj);
+            localStorage.notes = JSON.stringify(data);
+        },
+        getAllNotes: function() {
+            return JSON.parse(localStorage.notes);
+        }
+    };
+
+
+    var octopus = {
+        addNewNote: function(noteStr) {
+            model.add({
+                content: noteStr
+            });
+            view.render();
+        },
+
+        getNotes: function() {
+            return model.getAllNotes();
+        },
+
+        init: function() {
+            model.init();
+            view.init();
+        }
+    };
+
+
+    var view = {
+        init: function() {
+            this.noteList = $('#notes');
+            var newNoteForm = $('#new-note-form');
+            var newNoteContent = $('#new-note-content');
+            newNoteForm.submit(function(e){
+            var dateObj = new Date();
+            var month = dateObj.getUTCMonth() + 1; //months from 1-12
+            var day = dateObj.getUTCDate();
+            var year = dateObj.getUTCFullYear();
+
+            curdate = year + "/" + month + "/" + day;
+                octopus.addNewNote(newNoteContent.val() + ' '+ curdate);
+                newNoteContent.val('');
+                e.preventDefault();
+            });
+            view.render();
+        },
+        render: function(){
+            var htmlStr = '';
+            octopus.getNotes().forEach(function(note){
+                htmlStr += '<li class="note">'+
+                        note.content +
+                    '</li>';
+            });
+            this.noteList.html( htmlStr );
+        }
+    };
+
+    octopus.init();
+});
