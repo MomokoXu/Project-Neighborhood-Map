beb848a9ba91052a972808e265a24db8d2b01ea9
MomokoXu <momoko0808@hotmail.com>
2017-06-21 16:44:09 -0700

    seperate model and cat

--- Lessons_with_code/Cat_Cliker_Mini_Project/js/app.js
+++ Lessons_with_code/Cat_Cliker_Mini_Project/js/app.js
@@ -1,23 +1,39 @@
-var ViewModel = function() {
+var Cat = function() {
       this.name = ko.observable("Kitten1");
-      this.http = ko.observable("https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/");
+      this.imgSrc = ko.observable("https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/");
       this.clicks = ko.observable(0);
-      this.level = ko.computed(function() {
-      	if (this.clicks() <= 10) return "Newborn";
-      	else if (this.clicks() <= 30) return "Infant";
-      	else if (this.clicks() <= 50) return "Baby";
-      	else if (this.clicks() <= 50) return "Teen";
-      	else return "Growup";
-      }, this);
-
-      this.incrementCounter = function() {
-      	this.clicks(this.clicks() + 1);
-      };
 
       this.nickNames = ko.observableArray([
       	{name: "Cute kitten"},
       	{name: "Soft kitten"},
       ]);
+
+      this.level = ko.computed(function() {
+      	var title;
+      	var clicks = this.clicks();
+      	if (clicks < 10) {
+      		title = "Newborn";
+  		} else if (clicks < 50) {
+  			title = "Infant";
+		} else if (clicks < 100) {
+			title = "Child";
+		} else if (clicks < 200) {
+			title = "Teen";
+		} else if (clicks < 500) {
+			title = "Adult";
+		} else {
+			title = "Ninja"
+		}
+		return title;
+      }, this);
+}
+
+var ViewModel = function() {
+	this.currentCat = ko.observable(new Cat());
+	this.incrementCounter = function() {
+		this.currentCat().clicks(this.currentCat().clicks() + 1);
+	};
+
 }
 
 ko.applyBindings(new ViewModel());
